c0a4193cfe679aca3771df7ac3f83bb5a889e98f
Irving Cabello <irvollo@gmail.com>
2018-03-31 19:00:55 -0600

    Random post every 10 minutes

--- functions/index.js
+++ functions/index.js
@@ -22,23 +22,48 @@ var getJSON = function(url, callback) {
     xhr.send();
 };
 
-//var options = {screen_name: 'irvollo', count: 10}
+var parseTwit = function(post) {
+    return post.data.title + ' https://reddit.com' + post.data.permalink
+}
+
+var isAlreadyPost = function(post, tweets) {
+    var alreadyTwit = false;
+    for (var i = 0; i < tweets.length ; i++) {
+        if (!alreadyTwit && tweets[i].text.includes(post.data.title)) {
+            return true;
+        } else {
+            alreadyTwit = false;
+        }
+    }
+    return alreadyTwit;
+}
+
+var options = { screen_name: 'CC_Reddit',
+                count: 15 };
 
 exports.tweetBest = functions.https.onRequest((req, res) => {
     getJSON('https://www.reddit.com/r/cryptocurrency/top.json', function(err, data) {
         if (err !== null) {
             return res.send(err);
         } else {
-            var bestPost = data.data.children[1]
-            var toPost = bestPost.data.title + ' https://reddit.com' + bestPost.data.permalink
-            Twitter.post('statuses/update', {status: toPost},  function(error, tweet, response){
-                if(error){
-                  console.log(error);
+            var bestPost = data.data.children[Math.floor((Math.random() * 25) + 1)]
+            Twitter.get('statuses/user_timeline', options, function(err, data) {
+                var isPost = isAlreadyPost(bestPost, data); 
+                if (!isPost) {
+                    var toPost = parseTwit(bestPost);
+                    Twitter.post('statuses/update', {status: toPost},  function(error, tweet, response){
+                        if(error){
+                          console.log(error);
+                        }
+                        console.log(tweet);  // Tweet body.
+                        console.log(response);
+                        return res.send(bestPost);  // Raw response object.
+                    }); 
+                } else {
+                    // Try with the next Reddit Post.
+                    return res.send('Will try with another post')
                 }
-                console.log(tweet);  // Tweet body.
-                console.log(response);
-                return res.send(bestPost);  // Raw response object.
-            });
+            })
         }
     });
 })
